b76e7d1224189efd7364481075a99dcc9a4ef140
tebby24 <tvgonyea@iu.edu>
2024-04-03 08:51:00 -0400

    added nvim config

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        build = ":Copilot auth",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = false,
+                debounce = 75,
+                keymap = {
+                    accept = "<C-\\>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<C-]>",
+                    prev = "<C-[>",
+                    dismiss = "<Esc>",
+                },
+            },
+            -- panel = { enabled = false },
+
+            filetypes = {
+                markdown = true,
+                help = true,
+            },
+        },
+    },
+}
